455ea17344fe3a85bbb9b03a117315eacb1a0498
guerreroaanathalia@gmail.com <guerreroaanathalia@gmail.com>
2024-02-08 16:57:58 -0500

    backend agregado

---
+++ Proyecto_Integrador_Front/scripts/products.js
@@ -0,0 +1,90 @@
+// alert("conect js products")
+
+import {listaProductos} from '../module/products_module.js'
+import { filtrarPorTipo,buscarPorNombre } from '../module/functions_module.js';
+
+// const prod=[]
+
+const buttonAll =document.getElementById("all_products");
+const buttonRing =document.getElementById("rings");
+const buttonNecklace =document.getElementById("necklaces");
+const buttonEarring =document.getElementById("earrings");
+const buttonBracelet =document.getElementById("bracelets");
+
+const conteinerCards=document.getElementById("container_products");
+conteinerCards.innerHTML=``;
+const showCards =(products) =>{
+    // console.log(products,"products")
+    if(products.length === 0){
+        conteinerCards.innerHTML=`
+        <div class="empty_products">
+            <p>There are no products to show</p>
+        </div>
+        `
+    }
+    else{
+        let html=``
+        products.forEach(product => {
+            html += `
+            <div class="cards">
+                <img
+                src="${product.imagenes[0]}"
+                alt="${product.nombre}"
+                />
+                <h3>${product.nombre}</h3>
+                <p>$ ${product.precioUnitario}</p>
+            </div>
+            `
+        });
+        conteinerCards.innerHTML=html;
+    }
+}
+
+const inputSerch= document.getElementById("search_product");
+const buttonSerch= document.getElementById("btn_search");
+const searchProduct= (input,button) => {    
+    input.addEventListener('keyup',(event)=>{
+        if (event.code=== 'Enter') {
+           const productsbyName= buscarPorNombre(listaProductos, input.value);
+           showCards(productsbyName)      
+           conteinerCards.scrollIntoView();
+        }
+    });
+    button.addEventListener('click',()=>{
+           const productsbyName= buscarPorNombre(listaProductos, input.value);
+           showCards(productsbyName)      
+           conteinerCards.scrollIntoView();
+    });
+};
+searchProduct(inputSerch,buttonSerch);
+showCards(listaProductos);
+
+
+const showAll = (button) =>{
+    button.addEventListener("click", (event) => {
+        event.preventDefault();
+        showCards(listaProductos);
+    })
+}
+const showType = (button,term) =>{
+    button.addEventListener("click", (event) => {
+        event.preventDefault();
+        const filterProducts=filtrarPorTipo(listaProductos,term);
+        showCards(filterProducts);
+    })
+}
+
+showAll(buttonAll);
+showType(buttonRing,"ring");
+showType(buttonBracelet,"bracelet");
+showType(buttonEarring,"earring");
+showType(buttonNecklace,"Necklace");
+
+
+const selectElement = document.getElementById("filter_products");
+
+console.log("Texto seleccionado:", selectedText);
+
+const opt=selectElement.options[selectElement.selectedIndex].value;
+
+console.log(opt)
