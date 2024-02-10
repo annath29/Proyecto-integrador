66c787d238b6c9fbb09ff06e9357e2d751262467
annath29 <117115178+annath29@users.noreply.github.com>
2024-02-10 11:25:05 -0500

    inserte la estructura para hacer el flujo de details

---
+++ Proyecto_Integrador_Front/module/functions_fetch.js
@@ -0,0 +1,4 @@
+const url= 'https:/localhost:3300/products'
+
+const promesa=fetch(url)
+const respuesta = promesa.json();
--- Proyecto_Integrador_Front/scripts/details.js
+++ Proyecto_Integrador_Front/scripts/details.js
@@ -1,13 +1,15 @@
 import {showCart} from './modalCart.js';
 const btnBuy = document.getElementById("btn_buy");
 const btnAdd = document.getElementById("btn_add_cart");
-    btnBuy.addEventListener("click", () => {
+  btnBuy.addEventListener("click", () => {
        // localStorage.setItem("id_product", product.id);
       window.location.href = "../pages/order.html";
-    });
-    btnAdd.addEventListener("click", () => {
+   });
+  btnAdd.addEventListener("click", () => {
         // localStorage.setItem("id_product", product.id);
        //window.location.href = "../pages/order.html";
        showCart(btnAdd);
-       
-     });
+   });
+
+ const product = localStorage.getItem("id_product");
+ console.log(product,"product")
