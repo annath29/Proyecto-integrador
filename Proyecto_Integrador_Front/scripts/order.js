28b680fa9516f3232c416bb476a281b50e2f5c51
Deimer Amaya <damayaa@ufpso.edu.co>
2024-02-11 11:18:26 -0500

    .

--- Proyecto_Integrador_Front/scripts/order.js
+++ Proyecto_Integrador_Front/scripts/order.js
@@ -1,7 +1,19 @@
+
+import { listaProductos } from "../module/products_module.js";
+import { filtrarPorTipo, buscarPorNombre } from "../module/functions_module.js";
+
 const btnPurchase = document.getElementById("btn_purchase");
  btnPurchase.addEventListener("click", () => {
     // localStorage.setItem("id_product", product.id);
    window.location.href = "../pages/purchase.html";
+   
+   
+ });
+
+ const btnDelete = document.getElementById("btn_delete");
+ btnDelete.addEventListener("click", () => {
+  alert("entré");
+  
    
    
  });
